f4cd326df12821e0c4ec5638b51b6641a1a3b3f6
Jon Joe Joe <mike.porenta@gmail.com>
2024-10-17 17:45:00 -0600

    Update pnl_monitor.py
    Signed-off-by: Jon Joe Joe <mike.porenta@gmail.com>

--- src/tbot_tradingboat/pg_pnl_monitor/pnl_monitor.py
+++ src/tbot_tradingboat/pg_pnl_monitor/pnl_monitor.py
@@ -11,7 +11,7 @@ logger.add("pnl_monitor.log", rotation="1 MB")
 # Configuration variables (can be set via environment variables)
 IBKR_HOST = os.environ.get('IBKR_HOST', '127.0.0.1')
 IBKR_PORT = int(os.environ.get('IBKR_PORT', '4002'))
-CLIENT_ID = int(os.environ.get('CLIENT_ID', '1')) + 1  # Ensure unique client ID
+CLIENT_ID = int(os.environ.get('CLIENT_ID', '2')) + 1  # Ensure unique client ID
 PNL_THRESHOLD = float(os.environ.get('PNL_THRESHOLD', '-1.0'))  # Default to -1%
 ACCOUNT = os.environ.get('ACCOUNT', '')  # If not set, will use the first account
 
